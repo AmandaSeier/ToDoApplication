0cd8e4859a77dfbbe0fc1db98cbb32d30e30a0af
AmandaSeier <amandaseier1@gmail.com>
2026-07-18 21:19:35 +0200

    FinishedTasksView started and static UI finished for that view

--- obj/Debug/net8.0/ToDoApplication.AssemblyInfo.cs
+++ obj/Debug/net8.0/ToDoApplication.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ToDoApplication")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2672208ef9835c5479b016c3244050ecd4d5cc60")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+80e7711969ad654fbc0cecb612c53333d0afe887")]
 [assembly: System.Reflection.AssemblyProductAttribute("ToDoApplication")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ToDoApplication")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
